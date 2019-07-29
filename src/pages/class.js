597a47c7eb5576bdcaa3ac20f9e70a67215dbcef
Patrick B <patrick.barter@rogers.com>
2019-07-29 13:46:37 -0400

    Added buttons to each student to view their page

--- src/pages/class.js
+++ src/pages/class.js
@@ -2,19 +2,20 @@ import React from "react"
 import Layout from "../components/Layout"
 import style from "../styles/class.module.scss"
 import profile from "../images/profile.jpg"
+import { Link } from "gatsby"
+
 const Class = () => {
   return (
     <Layout>
       {/* <h2>Class List</h2>
       <p>Here's a list of students in a particular program. We should also have some ways to filter the students as well, perhaps by expertise/skills.</p>
       <p>Consider we could also show <em>all</em> students here from all programs who are participating, then filter by program (if desired). What are the benefits of either?</p> */}
-      <div className={style.classlistcont}>
+      <div className="classlistcont">
         <h1>Web Design and Development</h1>
         <br></br>
-        <div className={style.classlistdescription}>
+        <div className="classlistdescription">
         <h2>Class List</h2>
         <p>Here you will be able to view the 2019 Web Design and Development program students and see their preferred job positions.
-        Click on a students portrait shot for more information about them as well as a link to their portfolios or a preview of their school projects.
         </p>
         </div>
         <ul className ={style.studentlistcontainer}>
@@ -22,57 +23,97 @@ const Class = () => {
             <div id={style.studentimage}>
               <img src={profile}></img>
             </div>
-            <div className ={style.name}><p> Kyle  Brown</p></div>
-          < div className ={style.jobtitle}><p>Front-End Developer</p></div>
+            <div className={style.infocontainer}>
+              <div className ={style.name}><p> Kyle  Brown</p></div>
+              <div className ={style.jobtitle}><p>Front-End Developer</p></div>
+              <div className = {style.buttoncont}>
+              <button type="button" className="btn btn-outline-info mt-3 mb-4 btn-sm">More info</button>
+              </div>
+            </div>
           </li>
           <li className={style.student}>
             <div id={style.studentimage}>
               <img src={profile}></img>
             </div>
-            <div className ={style.name}><p> BHAVANA NAVULURI</p></div>
-          < div className ={style.jobtitle}><p>Back-End Developer</p></div>
+            <div className={style.infocontainer}>
+              <div className ={style.name}><p> Kyle  Brown</p></div>
+              <div className ={style.jobtitle}><p>Front-End Developer</p></div>
+              <div className = {style.buttoncont}>
+              <button type="button" className="btn btn-outline-info mt-3 mb-4 btn-sm">More info</button>
+              </div>
+            </div>
           </li>
           <li className={style.student}>
             <div id={style.studentimage}>
               <img src={profile}></img>
             </div>
-            <div className ={style.name}><p> BHAVANA NAVULURI</p></div>
-          < div className ={style.jobtitle}><p>Full Stack Developer</p></div>
+            <div className={style.infocontainer}>
+              <div className ={style.name}><p> Kyle  Brown</p></div>
+              <div className ={style.jobtitle}><p>Front-End Developer</p></div>
+              <div className = {style.buttoncont}>
+              <button type="button" className="btn btn-outline-info mt-3 mb-4 btn-sm">More info</button>
+              </div>
+            </div>
           </li>
           <li className={style.student}>
             <div id={style.studentimage}>
               <img src={profile}></img>
             </div>
-            <div className ={style.name}><p> BHAVANA NAVULURI</p></div>
-          < div className ={style.jobtitle}><p>Web Designer</p></div>
+            <div className={style.infocontainer}>
+              <div className ={style.name}><p> Kyle  Brown</p></div>
+              <div className ={style.jobtitle}><p>Front-End Developer</p></div>
+              <div className = {style.buttoncont}>
+              <button type="button" className="btn btn-outline-info mt-3 mb-4 btn-sm">More info</button>
+              </div>
+            </div>
           </li>
           <li className={style.student}>
             <div id={style.studentimage}>
               <img src={profile}></img>
             </div>
-            <div className ={style.name}><p> BHAVANA NAVULURI</p></div>
-          < div className ={style.jobtitle}><p>UI and UI Design</p></div>
+            <div className={style.infocontainer}>
+              <div className ={style.name}><p> Kyle  Brown</p></div>
+              <div className ={style.jobtitle}><p>Front-End Developer</p></div>
+              <div className = {style.buttoncont}>
+              <button type="button" className="btn btn-outline-info mt-3 mb-4 btn-sm">More info</button>
+              </div>
+            </div>
           </li>
           <li className={style.student}>
             <div id={style.studentimage}>
               <img src={profile}></img>
             </div>
-            <div className ={style.name}><p> BHAVANA NAVULURI</p></div>
-          < div className ={style.jobtitle}><p>Front-End Developer</p></div>
+            <div className={style.infocontainer}>
+              <div className ={style.name}><p> Kyle  Brown</p></div>
+              <div className ={style.jobtitle}><p>Front-End Developer</p></div>
+              <div className = {style.buttoncont}>
+              <button type="button" className="btn btn-outline-info mt-3 mb-4 btn-sm">More info</button>
+              </div>
+            </div>
           </li>
           <li className={style.student}>
             <div id={style.studentimage}>
               <img src={profile}></img>
             </div>
-            <div className ={style.name}><p> BHAVANA NAVULURI</p></div>
-          < div className ={style.jobtitle}><p>Front-End Developer</p></div>
+            <div className={style.infocontainer}>
+              <div className ={style.name}><p> Kyle  Brown</p></div>
+              <div className ={style.jobtitle}><p>Front-End Developer</p></div>
+              <div className = {style.buttoncont}>
+              <button type="button" className="btn btn-outline-info mt-3 mb-4 btn-sm">More info</button>
+              </div>
+            </div>
           </li>
           <li className={style.student}>
             <div id={style.studentimage}>
               <img src={profile}></img>
             </div>
-            <div className ={style.name}><p> BHAVANA NAVULURI</p></div>
-          < div className ={style.jobtitle}><p>Front-End Developer</p></div>
+            <div className={style.infocontainer}>
+              <div className ={style.name}><p> Kyle  Brown</p></div>
+              <div className ={style.jobtitle}><p>Front-End Developer</p></div>
+              <div className = {style.buttoncont}>
+              <button type="button" className="btn btn-outline-info mt-3 mb-4 btn-sm">More info</button>
+              </div>
+            </div>
           </li>
         </ul>
     </div>
