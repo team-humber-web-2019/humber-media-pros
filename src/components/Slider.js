b92698659c75177f2f70115b0ce55905c6fd112b
Ash-kan <ashkan.shokri@outlook.com>
2019-07-30 19:21:06 -0400

    removed btns

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -18,17 +18,17 @@ const Slider = () => {
         <div className={`carousel-inner ` + style.carousel + `-` + style.inner}>
           <div className={`item carousel-item active ` + style.item + style.carousel + `-` + style.item + style.active}>
             <div className={`img-box ` + style.imagee}><img className="rounded-circle" src={arath} alt="Arath"/></div><br/> 
-            <p className={`testimonial ` + style.testimonial }><small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Idac bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet.</small></p>
+            <p className={`testimonial ` + style.testimonial }><small>“Rocco is a great teacher…. In just four short months, I learned techniques that I will use over a lifetime to improve my web design skill.  This course is great for anyone.  I highly recommend it.”</small></p>
             <p className={`overview ` + style.overview}><b>Arath Yacob George</b>, Graphic Designer</p>
           </div>
           <div className={`item carousel-item ` + style.item + style.carousel + `-` + style.item}>
             <div className={`img-box ` + style.imagee}><img className="rounded-circle" src={ash} alt="Ash"/></div><br/> 
-            <p className={`testimonial ` + style.testimonial }><small>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</small></p>
+            <p className={`testimonial ` + style.testimonial }><small>“I’ve taken other development classes and this one was by far the most helpful and most practical. My computer skills improved dramatically over the last semester.”</small></p>
             <p className={`overview ` + style.overview}><b>Ashkan Shokri</b>, Web Developer</p>
           </div>
           <div className={`item carousel-item ` + style.item + style.carousel + `-` + style.item}>
             <div className={`img-box ` + style.imagee}><img className="rounded-circle" src={nina} alt="Nina"/></div><br/> 
-            <p className={`testimonial ` + style.testimonial }><small>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</small></p>
+            <p className={`testimonial ` + style.testimonial }><small>“Your class was something I looked forward to waking up to every week! I appreciate your generosity and your authenticity with your students.”</small></p>
             <p className={`overview ` + style.overview}><b>Nina Huang</b>, Web Developer</p>
           </div>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,8 +21,7 @@ const Index = () => {
    <div className="card text-center  " className={style.midcard} >
    <div className="card-body">
     <h5 className="card-title" className={style.midheading}>HUMBER MEDIA PROS GRAD SHOW 2019</h5><br/>
-    <p className="card-text"><small>This is the site landing page and will display the primary content banner for the event and all the important details about the event, location and what's being offered. Consider also including direct links to the three programs (for now, just linking them to the same page).</small></p>
-    <a href="#" class="btn btn-light rounded-0" >Read More</a>
+    <p className="card-text"><small>The Faculty of Media and Creative Arts invites you to attend our 2019 MEDIA PROS Grad Show. Join our students from the one-year intensive study programs in Graphic Design, Web Design and Development and 3D Modelling and Visual Effects; browse their portfolios, talk about their career goals and maybe find your next best employee.</small></p>
    </div>
    </div>
       
@@ -34,7 +33,7 @@ const Index = () => {
     <img src={graphicSvg} class="card-img-top" alt="graphicSvg"/>
     <div className="card-body ">
     <h5 className="card-title ">Graphic Design</h5>
-    <p className="card-text"><small>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</small></p>
+    <p className="card-text"><small>Motivated. Creative. Innovator. If these words describe you, then our intensive Graphic Design for Print & Web certificate program is for you.</small></p>
     <p class="card-text"><small class="text-muted">More</small></p> 
     </div>
     </Link>
@@ -45,7 +44,7 @@ const Index = () => {
     <img src={webSvg} class="card-img-top" alt="graphicSvg"/>
     <div className="card-body ">
     <h5 className="card-title ">Web Design</h5>
-    <p className="card-text"><small>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</small></p>
+    <p className="card-text"><small>If you describe yourself as creative, resourceful, detail-oriented and a problem solver, then Humber's Web Design and Development certificate program is for you.</small></p>
     <p class="card-text"><small class="text-muted">More</small></p> 
     </div>
     </Link>
@@ -56,7 +55,7 @@ const Index = () => {
     <img src={DSvg} class="card-img-top" alt="graphicSvg"/>
     <div className="card-body ">
     <h5 className="card-title ">3D Design</h5>
-    <p className="card-text"><small>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</small></p>
+    <p className="card-text"><small>Humber's 3D Modelling and Visual Effects Production certificate program focuses on both the conceptual and the technical, as you learn to master visual techniques to bring ideas to life.</small></p>
     <p class="card-text"><small class="text-muted">More</small></p> 
     </div>
     </Link>
