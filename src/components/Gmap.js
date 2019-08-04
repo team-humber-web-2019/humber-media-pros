c83b3b2c8b1181287a141b2365f117d2399848bc
juneate <roccopan@gmail.com>
2019-08-03 23:35:28 -0400

    Homepage cleanup

--- src/components/Gmap.js
+++ src/components/Gmap.js
@@ -4,20 +4,20 @@ import style from "../styles/index.module.scss"
 
 const Gmap = () => {
   return (
-    <>
-
-    <div className="card mb-3 text-center border-0" >
-      <iframe className={style.gmap} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2889.4722828418307!2d-79.52446178450406!3d43.59670717912325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b48363bb8245b%3A0xa3cba040ea95ec97!2sHumber+College+Art+Commons!5e0!3m2!1sen!2sca!4v1563983351977!5m2!1sen!2sca">
-      </iframe>    
-      <div className="card-body">
-      <br/><h5 className="card-title">Date And Time</h5>
-        <p className="card-text"><small>Thursday 15th August 2019 <br/> 5:00pm - 8:00pm</small></p><br/><br/>
-        <br/> <h5 className="card-title">Location</h5>
-        <p className="card-text"><small>3253 Lake Shore Blvd W, Humber College Art Commons, <br/>Torento ON M8V 1M2 </small></p><br/>
+    <section className={`row ${style.location}`}>
+      <div className="container-fluid mb-3 text-center border-0" >
+        <iframe className={`border-0 ${style.gmap}`} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2889.4722828418307!2d-79.52446178450406!3d43.59670717912325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b48363bb8245b%3A0xa3cba040ea95ec97!2sHumber+College+Art+Commons!5e0!3m2!1sen!2sca!4v1563983351977!5m2!1sen!2sca">
+        </iframe>  
+        <div>
+          <h5>Location</h5>
+          <p>3253 Lake Shore Blvd W, Humber College Art Commons, <br/>Torento ON M8V 1M2 </p>
+        </div>  
+        <div>
+          <h5>Date And Time</h5>
+          <p>Thursday 15th August 2019 <br/> 5:00pm - 8:00pm</p>
+        </div>
       </div>
-
-    </div>
-    </>
+    </section>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,57 +12,47 @@ import { Link } from "gatsby"
 
 import style from "../styles/index.module.scss"
 
-const Index = () => {
+const programs = [
+  {name: `Graphic Design`, img: graphicSvg, text: `Motivated. Creative. Innovator. If these words describe you, then our intensive Graphic Design for Print & Web certificate program is for you.`},
+  {name: `Web Design`, img: webSvg, text: `If you describe yourself as creative, resourceful, detail-oriented and a problem solver, then Humber's Web Design and Development certificate program is for you.`},
+  {name: `3D Design`, img: DSvg, text: `Humber's 3D Modelling and Visual Effects Production certificate program focuses on both the conceptual and the technical, as you learn to master visual techniques to bring ideas to life.`},
+];
+
+const programCards = programs.map(({name, img, text}, index) => {
   return (
-<Layout>
-
-  <img src={humberEventSvg} className={style.heroImag} alt="Responsive image" />
-
-  <div className="card text-center  " className={style.midcard} >
-    <div className="card-body">
-      <h5 className="card-title" className={style.midheading}>HUMBER MEDIA PROS GRAD SHOW 2019</h5><br/>
-      <p className="card-text">The Faculty of Media and Creative Arts invites you to attend our 2019 MEDIA PROS Grad Show. Join our students from the one-year intensive study programs in Graphic Design, Web Design and Development and 3D Modelling and Visual Effects; browse their portfolios, talk about their career goals and maybe find your next best employee.</p>
+    <div key={index} className="card border-0 shadow mb-5 bg-white rounded" >
+      <img src={img} className="card-img-top" alt={name}/>
+      <div className="card-body p-3">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">{text}</p>
+      </div>
+      <div class="card-footer text-muted">
+        <Link to="/program" className={style.midcardText}>Read More</Link> 
+      </div>
     </div>
-  </div>
-      
-  <section  className={style.midcardText}>
-    <div className="card-deck text-center">
+  )
+})
 
-      <div className="card border-0 shadow mb-5 bg-white rounded" >
-        <img src={graphicSvg} className="card-img-top" alt="graphicSvg"/>
-        <div className="card-body p-3">
-          <h5 className="card-title">Graphic Design</h5>
-          <p className="card-text">Motivated. Creative. Innovator. If these words describe you, then our intensive Graphic Design for Print & Web certificate program is for you.</p>
+const Index = () => {
+
+  return (
+
+    <Layout>
+
+      <img src={humberEventSvg} className={style.heroImag} alt="Responsive image" />
+
+      <div className="card text-center  " className={style.midcard} >
+        <div className="card-body">
+          <h5 className="card-title" className={style.midheading}>HUMBER MEDIA PROS GRAD SHOW 2019</h5><br/>
+          <p className="card-text">The Faculty of Media and Creative Arts invites you to attend our 2019 MEDIA PROS Grad Show. Join our students from the one-year intensive study programs in Graphic Design, Web Design and Development and 3D Modelling and Visual Effects; browse their portfolios, talk about their career goals and maybe find your next best employee.</p>
         </div>
-        <div class="card-footer text-muted">
-          <Link to="/program" className={style.midcardText}>More</Link> 
+      </div>
+        
+      <section  className={style.midcardText}>
+        <div className="card-deck text-center">
+          {programCards}
         </div>
-      </div>
-
-      <div className="card border-0 shadow p-3 mb-5 bg-white rounded" >
-      <Link to="/program" className={style.midcardText}>
-      <img src={webSvg} className="card-img-top" alt="graphicSvg"/>
-      <div className="card-body ">
-      <h5 className="card-title ">Web Design</h5>
-      <p className="card-text">If you describe yourself as creative, resourceful, detail-oriented and a problem solver, then Humber's Web Design and Development certificate program is for you.</p>
-      <p className="card-text">More</p> 
-      </div>
-      </Link>
-      </div>
-
-      <div className="card border-0 shadow p-3 mb-5 bg-white rounded">
-      <Link to="/program" className={style.midcardText}>
-      <img src={DSvg} className="card-img-top" alt="graphicSvg"/>
-      <div className="card-body ">
-      <h5 className="card-title ">3D Design</h5>
-      <p className="card-text">Humber's 3D Modelling and Visual Effects Production certificate program focuses on both the conceptual and the technical, as you learn to master visual techniques to bring ideas to life.</p>
-      <p className="card-text">More</p> 
-      </div>
-      </Link>
-      </div>
-    
-    </div>
-  </section>
+      </section>
 
       <Gmap />
       <Slider />
