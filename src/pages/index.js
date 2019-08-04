60e896305cdf9f8b1575cb0d477002f391167abb
juneate <roccopan@gmail.com>
2019-08-03 23:05:12 -0400

    x

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,52 +16,52 @@ const Index = () => {
   return (
 <Layout>
 
-   <img src={humberEventSvg} className={style.heroImag} alt="Responsive image" />
+  <img src={humberEventSvg} className={style.heroImag} alt="Responsive image" />
 
-   <div className="card text-center  " className={style.midcard} >
-   <div className="card-body">
-    <h5 className="card-title" className={style.midheading}>HUMBER MEDIA PROS GRAD SHOW 2019</h5><br/>
-    <p className="card-text"><small>The Faculty of Media and Creative Arts invites you to attend our 2019 MEDIA PROS Grad Show. Join our students from the one-year intensive study programs in Graphic Design, Web Design and Development and 3D Modelling and Visual Effects; browse their portfolios, talk about their career goals and maybe find your next best employee.</small></p>
-   </div>
-   </div>
+  <div className="card text-center  " className={style.midcard} >
+    <div className="card-body">
+      <h5 className="card-title" className={style.midheading}>HUMBER MEDIA PROS GRAD SHOW 2019</h5><br/>
+      <p className="card-text">The Faculty of Media and Creative Arts invites you to attend our 2019 MEDIA PROS Grad Show. Join our students from the one-year intensive study programs in Graphic Design, Web Design and Development and 3D Modelling and Visual Effects; browse their portfolios, talk about their career goals and maybe find your next best employee.</p>
+    </div>
+  </div>
       
   <section  className={style.midcardText}>
-  <div className="card-deck text-center " >
-   
-    <div className="card border-0 shadow p-3 mb-5 bg-white rounded" >
-    <Link to="/program" className={style.midcardText} >
-    <img src={graphicSvg} className="card-img-top" alt="graphicSvg"/>
-    <div className="card-body ">
-    <h5 className="card-title ">Graphic Design</h5>
-    <p className="card-text"><small>Motivated. Creative. Innovator. If these words describe you, then our intensive Graphic Design for Print & Web certificate program is for you.</small></p>
-    <p className="card-text"><small className="text-muted">More</small></p> 
-    </div>
-    </Link>
-    </div>
+    <div className="card-deck text-center">
 
-    <div className="card border-0 shadow p-3 mb-5 bg-white rounded" >
-    <Link to="/program" className={style.midcardText}>
-    <img src={webSvg} className="card-img-top" alt="graphicSvg"/>
-    <div className="card-body ">
-    <h5 className="card-title ">Web Design</h5>
-    <p className="card-text"><small>If you describe yourself as creative, resourceful, detail-oriented and a problem solver, then Humber's Web Design and Development certificate program is for you.</small></p>
-    <p className="card-text"><small className="text-muted">More</small></p> 
-    </div>
-    </Link>
-    </div>
+      <div className="card border-0 shadow mb-5 bg-white rounded" >
+        <img src={graphicSvg} className="card-img-top" alt="graphicSvg"/>
+        <div className="card-body p-3">
+          <h5 className="card-title">Graphic Design</h5>
+          <p className="card-text">Motivated. Creative. Innovator. If these words describe you, then our intensive Graphic Design for Print & Web certificate program is for you.</p>
+        </div>
+        <div class="card-footer text-muted">
+          <Link to="/program" className={style.midcardText}>More</Link> 
+        </div>
+      </div>
 
-    <div className="card border-0 shadow p-3 mb-5 bg-white rounded">
-    <Link to="/program" className={style.midcardText}>
-    <img src={DSvg} className="card-img-top" alt="graphicSvg"/>
-    <div className="card-body ">
-    <h5 className="card-title ">3D Design</h5>
-    <p className="card-text"><small>Humber's 3D Modelling and Visual Effects Production certificate program focuses on both the conceptual and the technical, as you learn to master visual techniques to bring ideas to life.</small></p>
-    <p className="card-text"><small className="text-muted">More</small></p> 
+      <div className="card border-0 shadow p-3 mb-5 bg-white rounded" >
+      <Link to="/program" className={style.midcardText}>
+      <img src={webSvg} className="card-img-top" alt="graphicSvg"/>
+      <div className="card-body ">
+      <h5 className="card-title ">Web Design</h5>
+      <p className="card-text">If you describe yourself as creative, resourceful, detail-oriented and a problem solver, then Humber's Web Design and Development certificate program is for you.</p>
+      <p className="card-text">More</p> 
+      </div>
+      </Link>
+      </div>
+
+      <div className="card border-0 shadow p-3 mb-5 bg-white rounded">
+      <Link to="/program" className={style.midcardText}>
+      <img src={DSvg} className="card-img-top" alt="graphicSvg"/>
+      <div className="card-body ">
+      <h5 className="card-title ">3D Design</h5>
+      <p className="card-text">Humber's 3D Modelling and Visual Effects Production certificate program focuses on both the conceptual and the technical, as you learn to master visual techniques to bring ideas to life.</p>
+      <p className="card-text">More</p> 
+      </div>
+      </Link>
+      </div>
+    
     </div>
-    </Link>
-    </div>
-  
-  </div>
   </section>
 
       <Gmap />
